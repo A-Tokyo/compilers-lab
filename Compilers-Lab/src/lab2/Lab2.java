a217229684c82836066e0218bb2eec434abd339d
Tokyo <ahmed.tokyo1@gmail.com>
2018-02-11 01:13:57 +0200

    refactor

--- Compilers-Lab/src/lab2/Lab2.java
+++ Compilers-Lab/src/lab2/Lab2.java
@@ -1,5 +1,7 @@
 package lab2;
 
 public class Lab2 {
-	
+	public static void runTask(){
+		
+	}
 }
--- Compilers-Lab/src/main/Main.java
+++ Compilers-Lab/src/main/Main.java
@@ -4,9 +4,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import lab1.Lab1;
+import lab2.Lab2;
 
 public class Main {
 	public static void main(String [] args) throws FileNotFoundException, IOException {
 		Lab1.runTask();
+		Lab2.runTask();
 	}
 }
